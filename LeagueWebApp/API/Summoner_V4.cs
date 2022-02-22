ca308f69c2127949cb2075528deb077e2c1e93fe
wojwos <wojwos98@gmail.com>
2022-02-23 00:41:23 +0100

    added search form and connection with api

---
+++ LeagueWebApp/API/Summoner_V4.cs
@@ -0,0 +1,31 @@
+﻿using LeagueWebApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueWebApp.API
+{
+    public class Summoner_V4 : Api
+    {
+        public Summoner_V4(string region) : base(region)
+        {
+        }
+        public SummonerDTO GetSummonerByName(string summonerName)
+        {
+            string path = "summoner/v4/summoners/by-name/" + summonerName;
+
+            var response = GET(GetURL(path));
+            string content = response.Content.ReadAsStringAsync().Result;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return JsonConvert.DeserializeObject<SummonerDTO>(content);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- LeagueWebApp/Controllers/ProfileController.cs
+++ LeagueWebApp/Controllers/ProfileController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LeagueWebApp.API;
+using LeagueWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,32 @@ namespace LeagueWebApp.Controllers
 {
     public class ProfileController : Controller
     {
+        public SummonerDTO Summoner { get; set; }
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(string searchName)
+        {
+            if (searchName == null)
+            {
+                return NotFound();
+            }
+
+
+            //get summoner from api
+            Summoner_V4 summoner_v4 = new Summoner_V4("EUW1");
+            Summoner = summoner_v4.GetSummonerByName(searchName);
+
+
+            if (Summoner == null)
+            {
+                //summoner doesnt exist
+                return NotFound();
+            }
+            return View(Summoner);
+        }
     }
 }
